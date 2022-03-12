4523cd5512ed8b820d420ee4ce3208bf54e77c2f
Jed <thanhtrung1012000@gmail.com>
2022-03-12 12:54:20 +0700

    feature: add backend stripe checkout

--- src/components/StripeCheckoutButton/index.js
+++ src/components/StripeCheckoutButton/index.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import StripeCheckout from 'react-stripe-checkout';
-import './index.scss';
+import axios from "axios";
+import PropTypes from "prop-types";
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import "./index.scss";
 
 function StripeCheckoutButton({ price }) {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_51KaMHIJc9rScA1Fb8rX9H51cahLih3GEwCPyAZvBX8imimxjkskxWQJHLqGcYYDerQ26I2bYFCET9irffQGAWfYt00VwGOv8UB';
-  const onToken = token => {
-    console.log(token);
-    alert("Payment Successful!");
-  }
+  const publishableKey =
+    "pk_test_51KaMHIJc9rScA1Fb8rX9H51cahLih3GEwCPyAZvBX8imimxjkskxWQJHLqGcYYDerQ26I2bYFCET9irffQGAWfYt00VwGOv8UB";
+  const onToken = async (token) => {
+    try {
+      await axios.post("payment", { amount: priceForStripe, token });
+      alert("Payment Successful");
+    } catch (error) {
+      console.log("Payment error: ", JSON.parse(error));
+      alert(
+        "There was an issue with your payment. Please sure you use the provided credit cart."
+      );
+    }
+  };
 
   return (
     <StripeCheckout
@@ -24,11 +33,11 @@ function StripeCheckoutButton({ price }) {
       token={onToken}
       stripeKey={publishableKey}
     />
-  )
+  );
 }
 
 StripeCheckoutButton.propTypes = {
   price: PropTypes.number.isRequired,
-}
+};
 
-export default StripeCheckoutButton
+export default StripeCheckoutButton;
